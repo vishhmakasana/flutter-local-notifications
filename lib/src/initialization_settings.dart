7e5eb7a7b682787e92e27adc2bc7580e6fabdb01
vishalpatel <vishal.makasana@hotmail.com>
2022-05-09 17:34:10 +0530

    Intial commit

---
+++ lib/src/initialization_settings.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_local_notifications_linux/flutter_local_notifications_linux.dart';
+
+import 'platform_specifics/android/initialization_settings.dart';
+import 'platform_specifics/ios/initialization_settings.dart';
+import 'platform_specifics/macos/initialization_settings.dart';
+
+/// Settings for initializing the plugin for each platform.
+class InitializationSettings {
+  /// Constructs an instance of [InitializationSettings].
+  const InitializationSettings({
+    this.android,
+    this.iOS,
+    this.macOS,
+    this.linux,
+  });
+
+  /// Settings for Android.
+  final AndroidInitializationSettings? android;
+
+  /// Settings for iOS.
+  final IOSInitializationSettings? iOS;
+
+  /// Settings for macOS.
+  final MacOSInitializationSettings? macOS;
+
+  /// Settings for Linux.
+  final LinuxInitializationSettings? linux;
+}
